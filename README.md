77a8319f0aa3764a5a1aa383b74173d53cca7f03
grahamna4 <nate.a.graham@gmail.com>
2022-05-21 17:14:14 -0700

    finished small app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # personalFileSorter
- sort files from a downloads/keep folder to relevant pernament folders
+ sort files from a downloads/keep folder to relevant permanent folders
+
+ This is primarily to learn the python language and create a small useful script
+
+
+# python --version
+Python 3.9.7
+
+# OS
+Windows 10
+
+# possible future extensions
+implement watchdog to detect when something is downloaded/put into downloads/keep and have it autorun
---
+++ downloadSort.py
@@ -0,0 +1,28 @@
+import os
+from shutil import move
+
+
+sourceDir = "C:\\Users\\natea\\Downloads\\keep"
+destMusic = "C:\\Users\\natea\\Music"
+destImg = "C:\\Users\\natea\\OneDrive\\Pictures\\temp"
+destPDF = "C:\\Users\\natea\\OneDrive\\Textbooks\\temp"
+        
+        
+        
+class moverHandler():
+        with os.scandir(sourceDir) as files:
+            for file in files:
+                fileName = file.name
+                dest = sourceDir
+                theFile = dest +"\\"+ file.name
+                if  fileName.endswith(".png") or fileName.endswith(".jpg") or fileName.endswith(".jpeg"):
+                    dest = destImg
+                    move(theFile, dest)
+                elif fileName.endswith(".mp3") or fileName.endswith(".wav"):
+                    dest = destMusic
+                    move(theFile, dest)
+                elif fileName.endswith(".pdf"):
+                    dest = destPDF
+                    move(theFile, dest)
+                print(fileName +" move to " + dest +" from " + sourceDir)
+moverHandler()
